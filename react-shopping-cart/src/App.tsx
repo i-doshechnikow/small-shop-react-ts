d47afb90a96b92829536cf7e10c21afeb8dd6962
i-doshechnikow <iliya.yurievich@yandex.ru>
2021-12-29 23:08:25 +0300

    added a request for data, form the application framework

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+
+import { Drawer, Grid, LinearProgress, Badge } from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+import { Wrapper } from "./App.styles";
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
 const App = () => {
-  return <div className="App">Lets start</div>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+
+  console.log("data :>> ", data);
+
+  const getTotalitems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveFromCart = () => null;
+  if (error) <div>Something went wrong...</div>;
+  return (
+    <div className="App">{isLoading ? <LinearProgress /> : "Lets start"}</div>
+  );
 };
 
 export default App;
